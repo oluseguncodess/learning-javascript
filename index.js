339d6dbb727a9b3b7eb8da9929473e810fbd799b
olusegun <olusegunbamgbelu755@gmail.com>
2024-02-05 23:34:09 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// variables 
+
+// Variables are ued to store data. Examples are below;
+
+var name = "name"; 
+
+// Console Log
+
+// This is used to display results on the console. 
+
+console.log (name);
