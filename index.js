7bf578d1e0df6065657b314303c7a2696595476b
olusegun <olusegunbamgbelu755@gmail.com>
2024-02-06 23:39:27 +0100

    functions lol

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ console.log (name);
 
 // functions 
 
+// Functions are used to perform a task concurently 
+
+function addition() {
+    var a = 2;
+    var b = 3;
+
+    var c = a+b;
+
+    return c;
+}
+
+console.log(addition);
