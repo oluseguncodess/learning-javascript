3240e8070a516dfd8990ebcd7853e91b175f62ab
olusegun <olusegunbamgbelu755@gmail.com>
2024-04-04 23:59:33 +0100

    m

--- js/test.js
+++ js/test.js
@@ -1,19 +1,5 @@
 // Here is where we test, debug and console log our javascript code!!!
 
-const result = {
-    success: ["max-length", "no-amd", "prefer-arrow -functions"],
-    failure: ["no-var", "ar-on-top", "line-break"],
-    skipped: ["id-blacklist", "no-dup-keys"]
-};
+const person = (name, age, size ) => ({name, age, size }); 
 
-function makeList(arr) {
-    const resultDisplayArray = [];
-    for(i = 0; i < arr.length; i++) {
-        resultDisplayArray.push(`<li class = "text-warning>${arr[i]}</li>`);
-    }
-
-    return resultDisplayArray;
-}
-
-const resultDisplayArray = makeList(result.failure);
-console.log(resultDisplayArray); 
+console.log(person("Chidera", 19, 178));
