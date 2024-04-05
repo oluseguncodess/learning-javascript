e8232004ba7e524c7a336e664a10ad5f06dc3d62
olusegun <olusegunbamgbelu755@gmail.com>
2024-04-05 23:44:59 +0100

    m

--- js/test.js
+++ js/test.js
@@ -1,5 +1,5 @@
 // Here is where we test, debug and console log our javascript code!!!
 
-const person = (name, age, size ) => ({name, age, size }); 
+const person = (name, age, size ) => ({name, age, size });   
 
 console.log(person("Chidera", 19, 178));
