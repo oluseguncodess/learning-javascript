6488e9b480ac4fc80a3a1dfb802b8394a50f3dc3
olusegun <olusegunbamgbelu755@gmail.com>
2024-04-25 13:06:52 +0100

    continue building blackjack game

--- projects/blackjack-game/index.js
+++ projects/blackjack-game/index.js
@@ -6,24 +6,33 @@
 let firstCard = 10;
 let secondCard = 17;
 let sum = firstCard + secondCard;
-console.log(sum);
 let hasBlackJack = false;
 let isAlive = true;
 let message;
+let messageEl = document.querySelector("#message-el");
 
+let sumEl = document.querySelector("#sum-el")
 
+//Create a function the starts the game
+function startGame() {
 //Generate the logic for the game using if / else statements
-if(sum < 21) {
-    message = "Do you want to draw a new card? 🙂";
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
-    hasBlackJack = true;
-} else {
-    message = "You're out of the game! 😭";
-    isAlive = false;
+
+    //Render the sum of the first and second card.
+    sumEl.textContent += ` ${sum}`;
+
+    if(sum < 21) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+        messageEl.textContent = message;
 }
 
-console.log(message);
+
 
 
 
