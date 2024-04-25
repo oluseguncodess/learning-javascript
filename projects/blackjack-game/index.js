aad4822834db3e90407c59ae7594c7fd414ac96f
olusegun <olusegunbamgbelu755@gmail.com>
2024-04-25 17:02:09 +0100

    build a blackjack game
    found a way to display the generated card using the .join() method and stackoverflow

--- projects/blackjack-game/index.js
+++ projects/blackjack-game/index.js
@@ -4,21 +4,30 @@
 
 
 let firstCard = 10;
-let secondCard = 17;
+let secondCard = 4;
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message;
 let messageEl = document.querySelector("#message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
 
-let sumEl = document.querySelector("#sum-el")
 
 //Create a function the starts the game
 function startGame() {
+    renderGame();
+}
+
+function renderGame() {
 //Generate the logic for the game using if / else statements
 
     //Render the sum of the first and second card.
-    sumEl.textContent += ` ${sum}`;
+    sumEl.textContent = ` Sum: ${sum}`;
+
+    //display the both cards 
+    cardsEl.textContent = ` Cards: ${cards[0]} ${cards[1]}`;
 
     if(sum < 21) {
         message = "Do you want to draw a new card?";
@@ -30,6 +39,29 @@ function startGame() {
         isAlive = false;
     }
         messageEl.textContent = message;
+}
+
+//create a function for the new card button 
+
+function newCard() {
+    console.log("Drawing a new card from the deck");
+
+    let newCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
+    
+    cards.push(newCard)
+
+    sum += newCard
+
+    renderGame();
+
+    //display the randomly generated card
+    let cardd = [];
+    for (let i = 0; i < cards.length; i++) {
+        cardd.push(cards[i]);
+    }
+    let cardToString = cardd.join(' ');
+    cardsEl.textContent = `Cards: ${cardToString}`;
+
 }
 
 
