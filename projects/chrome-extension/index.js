feb111eca6f105030c763b6a3416a020478d6011
olusegun <olusegunbamgbelu755@gmail.com>
2024-04-30 01:30:22 +0100

    feature built, awaiting deployment

--- projects/chrome-extension/index.js
+++ projects/chrome-extension/index.js
@@ -6,11 +6,25 @@ const UlEl = document.querySelector("#ul-el");
 let myLeads = ["wwwww", "jsede"];
 // add event listener.
 inputBtn.addEventListener("click", () => {
-    myLeads.push(inputEl.value);
-    // console.log(myLeads);
+    
+    if (inputEl.value === "") {
+        alert("Input a website")
+    } else {
+        myLeads.push(inputEl.value);
+        inputEl.value = "";
+        renderLeads();
+    }
 });
 
+function renderLeads() {
+    let listItems = "";
 
-for (let i = 0; i < myLeads.length; i++) {
-    UlEl.innerHTML += `<li> ${myLeads[i]}</li>`;
-} 
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems  += `<li><a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a></li>`;  
+    } 
+    
+    UlEl.innerHTML = listItems
+}
+
+
+
