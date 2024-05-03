75363853ba5023977a971827d33fdab460f3f4d6
olusegun <olusegunbamgbelu755@gmail.com>
2024-05-03 15:32:54 +0100

    create a save tab button

--- projects/leads-tracker-chrome-extension/index.js
+++ projects/leads-tracker-chrome-extension/index.js
@@ -3,6 +3,7 @@ const inputBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
 const UlEl = document.querySelector("#ul-el");
 const deleteBtn = document.querySelector("#delete-btn");
+const saveTab = document.querySelector("#save-tab");
 
 // create an array where you save the leads!
 let myLeads = [];
@@ -11,37 +12,56 @@ let myLeads = [];
 //what's in the local storage!!!
 
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-console.log(leadsFromLocalStorage);
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
-    renderLeads()
-    console.log(myLeads);
+    render(myLeads)
 }
+const tabs = [
+    {url: "https://www.linkedin.com/in/olusegunbamgbelu/"}
+]
+//create an event listener for the tab button!!
 
-// add event listener to button!
+saveTab.addEventListener("click", () => {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+});
+
+// create an event listener for the input button!
 
 inputBtn.addEventListener("click", () => {
     
     if (inputEl.value === "") {
-        alert("Input a website")
+        alert("Input your leads")
     } else {
         myLeads.push(inputEl.value);
         inputEl.value = "";
         localStorage.setItem("myLeads", JSON.stringify(myLeads));
-        renderLeads();
+        render(myLeads);
     }
+});
+
+// create an eventlistener for the delete button!!
+
+deleteBtn.addEventListener("dblclick", ()=> {
+    // clear out local storage;
+    localStorage.clear();
+
+    // clear out my leads;
+    myLeads = [];
+    UlEl.innerHTML = "";
 });
 
 //  Create a function that renders the saved leads!!
 
-function renderLeads() {
+function render(leads) {
     let listItems = "";
 
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems  += `
             <li>
-                <a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a>
+                <a href="${leads[i]}" target="_blank">${leads[i]}</a>
             </li>
         `
         ; 
