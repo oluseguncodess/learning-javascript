f594deb2026f28c8e376b9889171909bffad078a
olusegun <olusegunbamgbelu755@gmail.com>
2024-12-22 18:44:28 +0100

    done with chukenorris
    what i majorly learnt here is using the fetch api to get jokes to display on the screen

---
+++ concepts/async/freecodecamp/lessons/test.js
@@ -0,0 +1,9 @@
+async function GetData() {
+    let endpoint = 'https://jsonplaceholder.typicode.com/users';
+    const request = await fetch(endpoint);
+    const response = await request.json();
+    console.log(response);
+  }
+  
+  GetData(); // call the function
+  
